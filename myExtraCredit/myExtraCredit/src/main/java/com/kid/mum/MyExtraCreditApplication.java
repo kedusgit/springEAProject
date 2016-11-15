80d8f51807151cd0342815d04a0e962453aaa10b
kedus <bekidus@gmail.com>
2016-11-14 23:31:30 -0600

    t

---
+++ myExtraCredit/myExtraCredit/src/main/java/com/kid/mum/MyExtraCreditApplication.java
@@ -0,0 +1,12 @@
+package com.kid.mum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyExtraCreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyExtraCreditApplication.class, args);
+	}
+}
---
+++ myExtraCredit/myExtraCredit/src/main/java/com/kid/mum/domain/Volunteer.java
@@ -0,0 +1,26 @@
+package com.kid.mum.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Volunteer {
+	@Id @GeneratedValue
+	int id;
+	String somenthing;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getSomenthing() {
+		return somenthing;
+	}
+	public void setSomenthing(String somenthing) {
+		this.somenthing = somenthing;
+	}
+	
+
+}
