ee89e1466fbfd5f4ab7724a429e1a6c906462901
kedus <bekidus@gmail.com>
2016-11-14 20:07:30 -0600

    after solving the class path error

--- CS544ExtraCredit/src/main/java/com/kid/mum/domain/Beneficiaries.java
+++ CS544ExtraCredit/src/main/java/com/kid/mum/domain/Beneficiaries.java
@@ -1,8 +1,23 @@
 package com.kid.mum.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "Beneficiariy", catalog = "cs544spring")
 public class Beneficiaries {
+	
+	@Id
+	@GeneratedValue
+int id;
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 }
